96b441bff991a648e4c2f8463a5ffa9b31671c41
Pinak Mehta <pinak@moweb.com>
2023-07-09 17:24:06 +0530

    feat: add flatlist with API call

--- App.tsx
+++ App.tsx
@@ -1,117 +1,78 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import {FlatList, Image, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {getRequest} from './utils/api-helpers/axios-api';
+import {isEmpty} from './utils/common-helper';
 
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+const App = () => {
+  const [products, setProducts] = useState<any>([]);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  useEffect(() => {
+    fetchProducts().then();
+  }, []);
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+  const fetchProducts = async () => {
+    const response = await getRequest('https://fakestoreapi.com/products');
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+    if (response.length > 0) {
+      setProducts(response);
+    }
+  };
 
-function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  const ProductListItem = ({product}: any) => {
+    return (
+      <>
+        <View style={styles.flatList}>
+          <Image
+            source={{uri: product.image}}
+            height={50}
+            width={50}
+            style={{borderRadius: 20}}
+          />
+          <View style={{flexDirection: 'column', marginStart: 10}}>
+            <Text style={{fontSize: 20}}>
+              {!product || isEmpty(product.title) ? '-' : product.title}
+            </Text>
+            <Text>{product.description}</Text>
+            <Text style={styles.price}>${product.price}</Text>
+          </View>
+        </View>
+      </>
+    );
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+      <View style={{backgroundColor: '#FFFFFF'}}>
+        {products.length > 0 && (
+          <FlatList
+            data={products}
+            renderItem={({item}: any) => <ProductListItem product={item} />}
+            keyExtractor={(item: any) => item.id}
+            ItemSeparatorComponent={() => (
+              <View style={{height: 2, backgroundColor: '#000000'}} />
+            )}
+          />
+        )}
+      </View>
+    </>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  flatList: {
+    flexDirection: 'row',
+    backgroundColor: '#70a1ff',
+    marginVertical: 10,
+    marginHorizontal: 16,
+    paddingBottom: 32,
+    borderRadius: 6,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
+  price: {
+    color: '#FF0000',
+    fontSize: 20,
+    fontWeight: 'bold',
   },
 });
 
