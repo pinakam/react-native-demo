50361b98388920b8dc404274d918d87ba6bc9b95
Pinak Mehta <pinak@moweb.com>
2023-07-09 16:35:41 +0530

    chore: add axios library

---
+++ utils/api-helpers/axios-api.ts
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const getRequest = async (URL: any) => {
+  const response: any = await axios.get(URL).catch(e => {
+    console.warn(e);
+  });
+
+  return response;
+};
