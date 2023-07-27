194fdbbf73d4a0114658a4fe216374e7ff7c98a3
Pinak Mehta <pinak@moweb.com>
2023-07-27 15:22:49 +0530

    feat: add splash screen

--- android/app/src/main/java/com/basicdemo/MainApplication.java
+++ android/app/src/main/java/com/basicdemo/MainApplication.java
@@ -1,5 +1,6 @@
 package com.basicdemo;
 
+import org.devio.rn.splashscreen.SplashScreenReactPackage;
 import android.app.Application;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
